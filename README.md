9f11056b95c937785d469b0fd9e431e52a030e21
tejitak <tejitak@gmail.com>
2014-12-13 13:25:16 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Samples of Vue.js Components
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp  = require('gulp');
+var runSequence = require('run-sequence');
+var jshint = require('gulp-jshint');
+var browserify = require('gulp-browserify');
+
+gulp.task('jshint', function() {
+    return gulp.src('public/js/teji/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['sample/**/*.js', 'components/**/*.vue'], ['jshint', 'browserify']);
+});
+
+gulp.task('browserify', function() {
+    gulp.src('sample/popup/main.js')
+        .pipe(browserify({transform: ["debowerify", "vueify"]}))
+        .pipe(gulp.dest('dist/popup'));
+});
+
+gulp.task('default', function(){
+    runSequence('jshint', 'browserify', 'watch');
+});
